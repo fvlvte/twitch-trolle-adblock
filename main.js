3b6ef62c05c8c2b81e7f26df2d69a33888f49a38
Kacper Fałat <me@falat.me>
2023-08-26 12:43:41 -0600

    trol builder

---
+++ main.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const listaTroli = [
+  "shadoweeee",
+  "piotrostroleka",
+  "jepetto"
+];
+
+const output = [];
+
+for(let trol of listaTroli)
+{
+  output.push(`www.twitch.tv##.seventv-chat-message-background :contains("${trol}")`);
+}
+
+fs.writeFileSync("trole.txt", output.join("\n"), "utf8");
